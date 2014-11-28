d86724accd157982ab2dfac347670275e5191951
damien-maussion <damien.maussion@hotmail.fr>
2014-11-28 16:34:08 +0000

    first commit

---
+++ Test_asm/src/TestClass.java
@@ -0,0 +1,21 @@
+
+public class TestClass {
+
+	private int nb =0;
+	private String str ="connard";
+	public int getNb() {
+		return nb;
+	}
+	public void setNb(int nb) {
+		this.nb = nb;
+	}
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	
+	
+		
+}
